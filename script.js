5c24be6e5e8a47954dc0264ba60c608434ffd685
khomutovb <48166365+khomutovb@users.noreply.github.com>
2020-02-28 01:27:53 +0200

    version 2 with jquery

--- script.js
+++ script.js
@@ -8,9 +8,28 @@ const classNames = {
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
-let lSLength = localStorage.length;
-let localValue;
 
+
+document.addEventListener("click", e => {
+  if (e.target.matches(".todo-delete")) {
+    if (confirm('Delete this task?')) {
+      el = e.target.parentElement;
+      el.parentElement.removeChild(el);
+      updateCount()
+      store()
+    }
+  }
+  if (e.target.matches(".todo-checkbox")) {
+    if (e.target.checked) {
+      store()
+      localStorage.setItem("checked", JSON.stringify(arr));
+      countCheckedTodo(uncheckedCountSpan)
+    } else {
+      store()
+      count(uncheckedCountSpan)
+    }
+  }
+}, false);
 
 function newTodo() {
   const todoText = prompt("Add a todo!")
@@ -18,95 +37,69 @@ function newTodo() {
   if (todoText != '') {
     todoElement = makeTodo(todoText)
     render(todoElement, list)
-    countTodo(itemCountSpan)
-    storage(todoText)
-    countUncheckedTodo(uncheckedCountSpan)
-    const checkbox = todoElement.children[1];
-    const buttonDelete = todoElement.lastChild;
-
-    checkbox.addEventListener("click", function (event) {
-      if (event.target.checked) {
-        countCheckedTodo(uncheckedCountSpan)
-      } else {
-        countUncheckedTodo(uncheckedCountSpan)
-      }
-    })
-    buttonDelete.addEventListener("click", function (event) {
-      if (confirm('Delete this task?')) {
-        el = event.target.parentElement;
-        el.parentElement.removeChild(el);
-        countCheckedTodo(uncheckedCountSpan);
-        countTodo(itemCountSpan)
-        localStorage.clear();
-      }
-    })
+    updateCount()
+    store();
   }
 }
-
 function makeTodo(todoText) {
   const li = document.createElement("li")
   addStyle(classNames.TODO_ITEM, li)
-
   const span = document.createElement("span")
   addStyle(classNames.TODO_TEXT, span)
-
   const buttonDelete = document.createElement("button");
   addStyle(classNames.TODO_DELETE, buttonDelete);
-
   const checkbox = document.createElement("input");
   checkbox.setAttribute("type", "checkbox")
   addStyle(classNames.TODO_CHECKBOX, checkbox)
-
   span.textContent = todoText
-
-  render(span, li)
   render(checkbox, li)
+  render(span, li)
   render(buttonDelete, li)
   return li
-
 }
-
-
-function render(e, destination) {
-  destination.appendChild(e)
+function render(element, destination) {
+  destination.appendChild(element)
 }
-
-
-function countTodo(e) {
-  e.innerHTML = list.childNodes.length
-}
-
 function count(e) {
   let count = Number(e.innerHTML)
   count++
   e.innerHTML = count.toString()
 }
-function countUncheckedTodo(e) {
-  count(e)
-}
-
-
 function countCheckedTodo(e) {
   let count = Number(e.innerHTML)
   count--
   e.innerHTML = count.toString()
 }
-
-function addStyle(style, e) {
-  e.setAttribute("class", style)
+function store() {
+  window.localStorage.myItemsList = list.innerHTML;
+  var arr = $('.todo-checkbox').map(function () {
+    return this.checked;
+  }).get();
+  localStorage.setItem("checked", JSON.stringify(arr));
 }
- function store() {
-    window.localStorage.myitems = list.innerHTML;
+function getStore() {
+  let storedValues = window.localStorage.myItemsList;
+  if (!storedValues) {
+    list.innerHTML = '';
   }
-
-function getValueAfterReload() {
-	if(lSLength > 0) {
-		for (i = 0; i < lSLength; i++) {
-      let key = localStorage.key(i);
-      var newLi = '<li class="todo-container"><span class="todo-text">' + localStorage[key] + '</span><input type="checkbox" class="todo-checkbox"><button class="todo-delete" onclick="addChild()"></button></li>';
-      list.insertAdjacentHTML('beforeend', newLi);
-		}
-	}
+  else {
+    list.innerHTML = storedValues;
+    var arr = JSON.parse(localStorage.getItem('checked')) || [];
+    arr.forEach(function (checked, i) {
+      $('.todo-checkbox').eq(i).prop('checked', checked);
+    });
+  }
 }
-getValueAfterReload();
+function addStyle(style, element) {
+  element.setAttribute("class", style)
+}
+function updateCount() {
+  itemCountSpan.innerHTML = list.childNodes.length
+  let allCheckbox = document.querySelectorAll('input[type="checkbox"]').length
+  let checkedCount = document.querySelectorAll('input[type="checkbox"]:checked').length
+  let uncheckedCount = allCheckbox - checkedCount
+  uncheckedCountSpan.innerHTML = uncheckedCount
+}
 
+getStore();
+updateCount()
